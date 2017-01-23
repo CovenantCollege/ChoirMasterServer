d1edc62372c33c81740f5115d47e61573df979ad
David Reed <dboyreed@charter.net>
2017-01-23 18:22:15 -0500

    Rewrite singerStore to use database

--- src/stores/singerStore.js
+++ src/stores/singerStore.js
@@ -1,56 +1,47 @@
-// TODO: replace with real database
-let singerId = 0;
-let singersDatabase = [];
+module.exports = function singerStore(db) {
+  function validateSinger(singerData) {
+    if (singerData == null || typeof singerData != 'object') {
+      throw new Error('Data for new singer not found');
+    }
 
-function validateSinger(singerData) {
-  if (singerData == null || typeof singerData != 'object') {
-    throw new Error('Data for new singer not found');
-  }
+    if (singerData.name == null) {
+      throw new Error('Singers must have a name');
+    }
 
-  // TODO: validate fields
-}
-
-function find(singerId) {
-  for (let singer of singersDatabase) {
-    if (singer.id === singerId) {
-      return singer;
+    if (singerData.height == null) {
+      throw new Error('Singers must hava a height');
     }
   }
 
-  throw new Error('Singer not found');
-}
+  async function find(singerId) {
+    let results = await db.query('SELECT * from singers WHERE id = ?', [singerId]);
 
-function findAll() {
-  return singersDatabase;
-}
+    if (results.length == 0) {
+      throw new Error('Singer not found');
+    }
 
-function insert(singerData) {
-  validateSinger(singerData);
-
-  singerData.id = singerId++;
-  singersDatabase.push(singerData);
-
-  return singerData;
-}
-
-function findIndex(singerId) {
-  let indexOfSinger = singersDatabase.findIndex(singer => singer.id == singerId);
-  if (indexOfSinger === -1) {
-    throw new Error('Singer not found');
+    return results[0];
   }
 
-  return indexOfSinger;
-}
+  async function findAll() {
+    return db.query('SELECT * from singers');
+  }
 
-function update(singerId, newFields) {
-  validateSinger(newFields);
+  async function insert(singerData) {
+    validateSinger(singerData);
 
-  newFields.id = singerId;
-  singersDatabase.splice(findIndex(singerId), 1, newFields);
-}
+    let result = await db.query('INSERT INTO singers (name, height) VALUES (?, ?)', [singerData.name, singerData.height]);
+    return result.insertId;
+  }
 
-function remove(singerId) {
-  singersDatabase.splice(findIndex(singerId), 1);
-}
+  async function update(singerId, singerData) {
+    validateSinger(singerData);
+    await db.query('UPDATE singers SET name = ?, height = ? WHERE ID = ?', [singerData.name, singerData.height, singerId]);
+  }
 
-module.exports = { find, findAll, insert, update, remove };
+  async function remove(singerId) {
+    await db.query('DELETE FROM singers WHERE id = ?', [singerId]);
+  }
+
+  return { find, findAll, insert, update, remove };
+};
