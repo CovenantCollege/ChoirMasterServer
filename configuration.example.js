02b6b070a0b7a511c21c1b6539c006ffe635d56a
David Reed <dboyreed@charter.net>
2017-01-25 09:19:52 -0500

    Update configuration to load heroku db settings

--- configuration.example.js
+++ configuration.example.js
@@ -1,3 +1,5 @@
+let process = require('process');
+
 module.exports = {
   'database': {
     'host': 'localhost',
@@ -10,3 +12,12 @@ module.exports = {
     'encryptionKey': 'changethisatonce'
   }
 };
+
+if (process.env.HEROKU) {
+  module.exports.database = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+  };
+}
