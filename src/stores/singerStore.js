c1d2fcc872b1f75b2a1d8fc8e963175fc3dfdc16
“jla-13” <jonathan.austin@covenant.edu>
2017-01-31 22:48:09 -0500

    Changed sql queries to match up with databse

--- src/stores/singerStore.js
+++ src/stores/singerStore.js
@@ -14,7 +14,7 @@ module.exports = function singerStore(db) {
   }
 
   async function find(singerId) {
-    let results = await db.query('SELECT * from singers WHERE id = ?', [singerId]);
+    let results = await db.query('SELECT * from Singers WHERE orgId = ?', [singerId]);
 
     if (results.length == 0) {
       throw new Error('Singer not found');
@@ -23,24 +23,24 @@ module.exports = function singerStore(db) {
     return results[0];
   }
 
-  async function findAll() {
-    return db.query('SELECT * from singers');
+  async function findAll(organizationData) {
+    return db.query('SELECT * from Singers WHERE orgId = ?', [organizationData.orgId]);
   }
 
   async function insert(singerData) {
     validateSinger(singerData);
 
-    let result = await db.query('INSERT INTO singers (name, height) VALUES (?, ?)', [singerData.name, singerData.height]);
+    let result = await db.query('INSERT INTO Singers (name, height, orgId) VALUES (?, ?, ?)', [singerData.name, singerData.height, singerData.orgId]);
     return result.insertId;
   }
 
   async function update(singerId, singerData) {
     validateSinger(singerData);
-    await db.query('UPDATE singers SET name = ?, height = ? WHERE ID = ?', [singerData.name, singerData.height, singerId]);
+    await db.query('UPDATE Singers SET name = ?, height = ? WHERE orgId = ?', [singerData.name, singerData.height, singerId]);
   }
 
   async function remove(singerId) {
-    await db.query('DELETE FROM singers WHERE id = ?', [singerId]);
+    await db.query('DELETE FROM Singers WHERE orgId = ?', [singerId]);
   }
 
   return { find, findAll, insert, update, remove };
