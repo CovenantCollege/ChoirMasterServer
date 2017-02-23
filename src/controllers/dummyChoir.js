49086eb1a6c6714054af80c3ba8014ce8c79178a
Marc Bohler <marc.bohler@covenant.edu>
2017-02-23 16:18:50 -0500

    Basic documentation for dummy choir

--- src/controllers/dummyChoir.js
+++ src/controllers/dummyChoir.js
@@ -1,3 +1,6 @@
+/**
+ * Returns dummy choir date without authentication
+ */
 module.exports = function dummyChoirController(app) {
   app.get('/dummy-choir', async (req, res) => {
     res.status(200).send([{ name: 'Marc Bohler', height: 69, voice: 'tenor', id: 123 }, { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 321 }]);
