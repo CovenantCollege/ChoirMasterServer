0e156dd1f14f6ae9d958fe1001cb1e31790dc7c7
Marc Bohler <marc.bohler@covenant.edu>
2017-02-26 22:08:09 -0500

    Added more dummy choir members

--- src/controllers/dummyChoir.js
+++ src/controllers/dummyChoir.js
@@ -3,6 +3,23 @@
  */
 module.exports = function dummyChoirController(app) {
   app.get('/dummy-choir', async (req, res) => {
-    res.status(200).send([{ name: 'Marc Bohler', height: 69, voice: 'tenor', id: 123 }, { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 321 }]);
+    res.status(200).send(
+      [
+        { name: 'Marc Bohler', height: 69, voice: 'tenor', id: 12, img: 'https://randomuser.me/api/portraits/med/men/' + 12 + '.jpg' }, 
+        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 32 + '.jpg' },
+        { name: 'Josh Humphries', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 33 + '.jpg' },
+        { name: 'Josh Foster', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 34 + '.jpg' },
+        { name: 'Ben Jobson', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 35 + '.jpg' },
+        { name: 'Abby Hynson', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/women/' + 7 + '.jpg' },
+        { name: 'David Reed', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 37 + '.jpg' },
+        { name: 'Spencer Dent', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 38 + '.jpg' },
+        { name: 'Nick Gilbert', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 39 + '.jpg' },
+        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 40 + '.jpg' },
+        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 41 + '.jpg' },
+        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 42 + '.jpg' },
+        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 43 + '.jpg' },
+        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 44 + '.jpg' },
+        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 45 + '.jpg' },
+        ]);
   });
 };
