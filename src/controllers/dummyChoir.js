25baa652a197bd4aae909d9794794ecfaf1bca04
Marc Bohler <marc.bohler@covenant.edu>
2017-02-27 02:48:46 -0500

    Drag and drop working

--- src/controllers/dummyChoir.js
+++ src/controllers/dummyChoir.js
@@ -5,21 +5,27 @@ module.exports = function dummyChoirController(app) {
   app.get('/dummy-choir', async (req, res) => {
     res.status(200).send(
       [
-        { name: 'Marc Bohler', height: 69, voice: 'tenor', id: 12, img: 'https://randomuser.me/api/portraits/med/men/' + 12 + '.jpg' }, 
-        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 32 + '.jpg' },
-        { name: 'Josh Humphries', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 33 + '.jpg' },
-        { name: 'Josh Foster', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 34 + '.jpg' },
-        { name: 'Ben Jobson', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 35 + '.jpg' },
-        { name: 'Abby Hynson', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/women/' + 7 + '.jpg' },
-        { name: 'David Reed', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 37 + '.jpg' },
-        { name: 'Spencer Dent', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 38 + '.jpg' },
-        { name: 'Nick Gilbert', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 39 + '.jpg' },
-        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 40 + '.jpg' },
-        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 41 + '.jpg' },
-        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 42 + '.jpg' },
-        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 43 + '.jpg' },
-        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 44 + '.jpg' },
-        { name: 'Jonathan Austin', height: 72, voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 45 + '.jpg' },
-        ]);
+        [
+          { name: 'Marc Bohler', height: "5' 9\"", voice: 'tenor', id: 12, img: 'https://randomuser.me/api/portraits/med/men/' + 12 + '.jpg', row: 0, col: 0}, 
+          { name: 'Jonathan Austin', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 32 + '.jpg', row: 0, col: 1 },
+          { name: 'Josh Humphries', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 33 + '.jpg', row: 0, col: 2 },
+          { name: 'Josh Foster', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 34 + '.jpg', row: 0, col: 3 },
+          { name: 'Ben Jobson', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 35 + '.jpg', row: 0, col: 4 },
+        ],
+        [
+          { name: 'Abby Hynson', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/women/' + 7 + '.jpg', row: 1, col: 0 },
+          { name: 'David Reed', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 37 + '.jpg', row: 1, col: 1 },
+          { name: 'Spencer Dent', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 38 + '.jpg', row: 1, col: 2 },
+          { name: 'Nick Gilbert', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 39 + '.jpg', row: 1, col: 3 },
+          { name: 'Jonathan Austin', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 40 + '.jpg', row: 1, col: 4 },
+        ],
+        [
+          { name: 'Jonathan Austin', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 41 + '.jpg', row: 2, col: 0 },
+          { name: 'Jonathan Austin', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 42 + '.jpg', row: 2, col: 1 },
+          { name: 'Jonathan Austin', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 43 + '.jpg', row: 2, col: 2 },
+          { name: 'Jonathan Austin', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 44 + '.jpg', row: 2, col: 3 },
+          { name: 'Jonathan Austin', height: "6' 0\"", voice: 'alto', id: 32, img: 'https://randomuser.me/api/portraits/med/men/' + 45 + '.jpg', row: 2, col: 4 },
+        ]
+      ]);
   });
 };
