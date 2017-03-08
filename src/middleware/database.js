d76681227ddd5b968ce2f31bf305240d482bd4c0
David Reed <dboyreed@charter.net>
2017-03-08 13:43:04 -0600

    Correctly report errors when opening database connection

--- src/middleware/database.js
+++ src/middleware/database.js
@@ -1,13 +1,15 @@
 let Database = require('../modules/database.js');
 
 module.exports = function (request, response, next) {
-  Database.connect().then(connection => {
-    request.db = connection;
+  Database.connect()
+    .then(connection => {
+      request.db = connection;
 
-    request.on('end', () => {
-      request.db.close();
-    });
+      request.on('end', () => {
+        request.db.close();
+      });
 
-    next();
-  });
+      next();
+    })
+    .catch(next);
 };
