c51b560821c6c873d7d7f9bdf6e678ff87b6b0cd
David Reed <dboyreed@charter.net>
2017-04-09 16:13:53 -0400

    Add API for enabling and disabling grid spots

--- src/controllers/grid.js
+++ src/controllers/grid.js
@@ -11,4 +11,15 @@ module.exports = function gridController(app) {
 
     res.status(204).send({});
   });
+
+  app.get('/organizations/:orgId/performances/:performanceId/grid/singers', async (req, res) => {
+    let grid = await req.grid.findSingers(req.params.performanceId);
+    res.status(200).send(grid);
+  });
+
+  app.put('/organizations/:orgId/performances/:performanceId/grid/singers', async (req, res) => {
+    await req.grid.updateSingers(req.params.performanceId, req.body);
+
+    res.status(204).send({});
+  });
 };
--- src/stores/gridStore.js
+++ src/stores/gridStore.js
@@ -1,35 +1,71 @@
 let { ValidationError/*, NotFoundError*/ } = require('./errors.js');
 let Store = require('./store.js');
 
-function validateGrid(grid, maxX, maxY) {
+function validateGrid(grid) {
   if (!Array.isArray(grid)) {
     throw new ValidationError('Grid must be an array of grid spots');
   }
 
-  for (let gridSinger of grid) {
-    if (!Number.isInteger(gridSinger.x)) {
-      throw new ValidationError('Every singer in the grid must have an x coordinate');
+  for (let gridSquare of grid) {
+    if (!Number.isInteger(gridSquare.x)) {
+      throw new ValidationError('Every square in the grid must have an x coordinate');
     }
 
-    if (!Number.isInteger(gridSinger.y)) {
-      throw new ValidationError('Every singer in the grid must have an y coordinate');
-    }
-
-    if (gridSinger.x > maxX || gridSinger.y > maxY) {
-      throw new ValidationError('Grid contains an out of bounds singer');
+    if (!Number.isInteger(gridSquare.y)) {
+      throw new ValidationError('Every square in the grid must have an y coordinate');
     }
   }
 }
 
 class GridStore extends Store {
   async find(performanceId) {
-    return await this.database.query('SELECT * FROM Grid WHERE performanceId = ?', [performanceId]);
+    return await this.database.query('SELECT x, y FROM E_Grid WHERE performanceId = ? AND ENABLED = 1', [performanceId]);
   }
 
   async update(performanceId, newGrid) {
     validateGrid(newGrid);
 
-    let existingGridQuery = await this.find(performanceId);
+    let existingGrid = await this.find(performanceId);
+
+    let spotsToDisable = existingGrid.filter(oldSpot => {
+      for (let newSpot of newGrid) {
+        if (newSpot.x === oldSpot.x && newSpot.y === oldSpot.y) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+
+    let spotsToEnable = newGrid.filter(newSpot => {
+      for (let oldSpot of existingGrid) {
+        if (oldSpot.x === newSpot.x && oldSpot.y === newSpot.y) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+
+    for (let { x, y } of spotsToDisable) {
+      console.log("Disabling " + x + ", " + y);
+      await this.disableSpot(performanceId, x, y);
+    }
+
+    for (let { x, y } of spotsToEnable) {
+      console.log("Enabling " + x + ", " + y);
+      await this.enableSpot(performanceId, x, y);
+    }
+  }
+
+  async findSingers(performanceId) {
+    return await this.database.query('SELECT * FROM Grid WHERE performanceId = ?', [performanceId]);
+  }
+
+  async updateSingers(performanceId, newGrid) {
+    validateGrid(newGrid);
+
+    let existingGridQuery = await this.findSingers(performanceId);
 
     let singersInOldGrid = existingGridQuery.map(g => g.singerId);
     let singersInNewGrid = newGrid.map(g => g.singerId);
@@ -59,6 +95,20 @@ class GridStore extends Store {
       let newSinger = singersInNewGrid.find(s => s.id == singerIdToUpdate);
       await this.updateSinger(performanceId, singerIdToUpdate, newSinger.x, newSinger.y);
     }
+  }
+
+  async enableSpot(performanceId, x, y) {
+    await this.database.query(
+      'INSERT INTO E_Grid (performanceId, x, y, enabled) VALUES (?, ?, ?, 1)',
+      [performanceId, x, y]
+    );
+  }
+
+  async disableSpot(performanceId, x, y) {
+    await this.database.query(
+      'DELETE FROM E_Grid WHERE performanceId = ? AND x = ? AND y = ?',
+      [performanceId, x, y]
+    );
   }
 
   async addSinger(performanceId, singerId, x, y) {
